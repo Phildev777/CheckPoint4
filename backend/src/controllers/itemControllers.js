d397c19f9ae9756cf0579dba9047ec16fc6f52a7
Philippe Berra <berraphilippe07@gmail.com>
2023-01-29 22:08:56 +0100

    début MVC

--- backend/src/controllers/itemControllers.js
+++ backend/src/controllers/itemControllers.js
@@ -1,33 +1,49 @@
 const modelItem = require("../models/AbstractManager");
 
-const browse = (req, res) => {
-  modelItem.findAll()
-    .then(([result]) => {
-      res.status(200).send(result);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.sendStatus(500);
-    });
+const browse = async (req, res) => {
+ 
+ try{
+
+  const [result] = await modelItem.findAll()
+
+return res.status(200).send(result);
+
+
+}
+catch(err){
+  console.error(err);
+
+  return res.status(500).send("problem");
+}
+
+ 
+  
 };
 
 
-const read = (req, res) => {
-  modelItem.find(req.params.id)
-    .then(([rows]) => {
-      if (rows[0] == null) {
-        res.sendStatus(404);
-      } else {
-        res.send(rows[0]);
-      }
-    })
-    .catch((err) => {
-      console.error(err);
-      res.sendStatus(500);
-    });
-};
+const read= async (req, res)=> {
 
-const edit = (req, res) => {
+  try {
+    const [result] = await modelItem.find(req.params.id)//params fait référence aux : dans les routes
+
+
+    return res.status(200).send(result);
+
+
+
+} catch (err) {
+
+    console.error(err)
+    return res.status(500).send("problème")
+
+}
+
+
+
+
+}
+
+/* const edit = (req, res) => {
   const item = req.body;
 
   // TODO validations (length, format...)
@@ -47,39 +63,77 @@ const edit = (req, res) => {
       console.error(err);
       res.sendStatus(500);
     });
-};
+}; */
 
-const add = (req, res) => {
-  const item = req.body;
 
-  // TODO validations (length, format...)
+const edit = async (req, res) => {
 
-  models.item
-    .insert(item)
-    .then(([result]) => {
-      res.location(`/items/${result.insertId}`).sendStatus(201);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.sendStatus(500);
-    });
-};
+  try {
 
-const destroy = (req, res) => {
-  models.item
-    .delete(req.params.id)
-    .then(([result]) => {
-      if (result.affectedRows === 0) {
-        res.sendStatus(404);
+      const { name } = req.body;
+      const [ result ] = await modelItem.editor(name);
+      if (result.affectedRows > 0) {
+          return res.status(201).send(result);
+
+
       } else {
-        res.sendStatus(204);
+          return res.status(400).send("erreur")
       }
-    })
-    .catch((err) => {
-      console.error(err);
-      res.sendStatus(500);
-    });
+
+  } catch (err) {
+
+      console.error(err)
+      return res.status(500).send("problème")
+
+  } 
+
+}
+
+const add = async (req, res) => {
+  const {
+    name,
+   
+  } = req.body;
+
+  const result = await modelItem.create(
+    name,
+  
+  );
+
+  if (result === "Created") {
+    return res.status(201).send("Inscription effectuée");
+  }
+  if (result === "Not created") {
+    return res.status(200).send("Employé déjà inscrit");
+  }
+
+  return res.status(500).send("Something broke");
 };
+
+
+
+
+
+const destroy =async (req, res) => {
+
+  try {
+
+
+    const [ result ] = await modelItem.del(req.params.id)
+
+    if (result.affectedRows > 0) {
+        return res.status(400).send("utilisateur supprimé");
+
+    }
+
+} catch (err) {
+
+    console.error(err)
+    return res.status(500).send("problème")
+
+}
+}
+  
 
 module.exports = {
   browse,
--- backend/src/models/AbstractManager.js
+++ backend/src/models/AbstractManager.js
@@ -16,19 +16,54 @@ const connection = require('../config/db');
     return connection.query("select * from  kingdom");
   }
 
-  /* const delete =(id)=> {
-    return this.connection.query("delete from kingdom where id = ?", [
+   const del =(id)=> {
+    return connection.query("delete from kingdom where id = ?", [
       id,
     ]);
-  } */
+  } 
 
 
+
+
+  const create = async (
+    name,
+
+  ) => {
+    try {
+     /*  const hashedMotdepasse = await bcrypt.hashSync(
+        motdepasse,
+        process.env.SALT
+      ); */
+      const [result] = await connection.query(
+        "INSERT INTO kingdom ( name) VALUES (?)",
+        [
+          name,
+         
+        ]
+      );
+  
+      if (result.affectedRows > 0) {
+        return "Created";
+      }
+      return "Not created";
+    } catch (e) {
+      console.error(e);
+      return `Erreur : ${e}`;
+    }
+  };
+
+  const editor = (id,name) => {
+    return connection.query("UPDATE kingdom SET name=? WHERE id=?", [
+      name,
+      id
+    ]);
+  };
 
 
 module.exports = {
 find,
 findAll,
-
-
-
+del,
+create,
+editor,
 };
